22ada484910a879d82ff6c451f79d4909dae23d0
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2023-09-30 11:39:23 +0100

    feat(cli): add displaying of menu options

--- src/cli_class.py
+++ src/cli_class.py
@@ -1,20 +1,31 @@
 class CommandLineInterface:
     def __init__(self):
         self.__display_banner()
+        self.__main_menu()
 
     def __display_banner(self):
         ascii_art = """
          _                                             
         | |                                            
         | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
-        | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+        | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \  
         | | | | (_| | | | | (_| | | | | | | (_| | | | |
         |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
                             __/ |                      
                            |___/
-        """
+        """  # noqa: E701, W605, W291
 
         print(ascii_art)
+
+    def __main_menu(self):
+        self.__display_menu_options()
+
+    def __display_menu_options(self):
+        print("Menu Options")
+        print("------------")
+        print("1 - Local Multiplayer")
+        print("2 - Single player")
+        print("3 - Quit")
 
 
 if __name__ == "__main__":
